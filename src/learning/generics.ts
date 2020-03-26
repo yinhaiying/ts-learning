401f75e4b93e1a05dc98829ebe6bf50ee8d0ccf2
yinhaiying <yinhaiying@58.com>
2020-03-26 16:24:03 +0800

    '泛型'

--- src/learning/generics.ts
+++ src/learning/generics.ts
@@ -12,9 +12,6 @@ echo1(2);
 echo1('helllo');
 
 // 类型变量
-
-
-
 let echo2 = <T>(arg:T):T => {
     return arg;
 };
@@ -40,3 +37,71 @@ let echo4 = <T>(arg:T[]):T[] => {
     return arg;
 };
 console.log(echo4([1,2]));
+
+
+// 使用ES6来定义泛型函数类型
+
+let echo5 : <T>(arg:T[]) => T[];
+
+// 定义实际的函数
+console.log('泛型定义函数类型');
+echo5 = (arg:any) => {
+    return arg;
+};
+// echo5([1,2,3]).map((item) => item.length);
+
+
+// 使用type类型别名来定义泛型函数类型
+console.log('使用type类型别名来定义函数类型');
+type Echo = <T>(arg:T) => T;
+let echo6:Echo = (arg:any) => {
+    return arg;
+};
+echo6([1,2,3]).map((item) => item.toFixed());
+
+
+// 使用接口来定义泛型函数类型
+console.log('使用接口来定义函数类型');
+interface Echos{
+    <T>(arg:T):T
+};
+let echo7:Echo = (arg:any) => {
+    return arg;
+};
+console.log( echo6(['1','22','333']).map((item) => item.length));
+
+
+console.log('约束');
+
+
+// 类型约束
+interface valueWithLength {
+    length:number
+};
+
+let echo8 = <T extends valueWithLength>(arg:T) => {
+    console.log(arg.length);
+    return arg;
+};
+echo8('1');
+echo8([1,2,3]);
+
+// 类型约束中使用类型参数
+console.log('在类型约束中使用类型参数')
+const getProp = (obj:any,prop:any) => {
+    return obj[prop];
+};
+const obj = {
+    a:'a',
+    b:'b'
+};
+console.log(getProp(obj,'a'));  // a
+console.log(getProp(obj,'c'));  // undefined
+
+
+// 类型约束中使用类型参数
+const getProp2 = <T,U extends keyof T>(obj:T,prop:U) => {
+    return obj[prop];
+};
+console.log(getProp2(obj,'a'));  // a
+console.log(getProp2(obj,'c'));  // Argument of type '"c"' is not assignable to parameter of type '"a" | "b"'
